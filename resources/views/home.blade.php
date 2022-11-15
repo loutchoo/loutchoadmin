a756edc60b81cee80b69b09f0f54a288b08f782c
Loutcho_Q <63863060+loutchoo@users.noreply.github.com>
2022-11-15 19:29:12 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Loutchoadmin - Home</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div>
+        <h1 class="text-center text-red-600">hello world</h1>
+    </div>
+</body>
+</html>
