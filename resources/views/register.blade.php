592f109405f57b13237e8de7430490ab89d91c51
Loutcho_Q <63863060+loutchoo@users.noreply.github.com>
2022-11-21 13:20:44 +0100

    menu update for smartphones

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -33,28 +33,28 @@
             </div>
         <nav>
             <ul class="flex items-center justify-center font-semibold mx-10">
-                <li class="relative px-3 py-2">
+                <li class="relative sm:px-3 px-2 py-2">
                     <a href="/home">
                         <div class="bg-white rounded-full border-2 border-white text-white p-3 bg-opacity-5 hover:border-green-400">
                             <span class="hover:opacity-50 hover:text-green-400">Home</span>
                         </div>
                     </a>
                 </li>
-                <li class="relative px-3 py-2">
+                <li class="relative sm:px-3 px-2 py-2">
                     <a href="/todolist">
                         <div class="bg-white rounded-full border-2 border-white text-white p-3 bg-opacity-5 hover:border-green-400">
                             <span class="hover:opacity-50 hover:text-green-400">Todolist</span>
                         </div>
                     </a>
                 </li>
-                <li class="relative px-3 py-2">
+                <li class="relative sm:px-3 px-2 py-2">
                     <a href="/projects">
                         <div class="bg-white rounded-full border-2 border-white text-white p-3 bg-opacity-5 hover:border-green-400">
                             <span class="hover:opacity-50 hover:text-green-400">Projects</span>
                         </div>
                     </a>
                 </li>
-                <li class="relative px-3 py-2">
+                <li class="relative sm:px-3 px-2 py-2">
                     <a href="/about">
                         <div class="bg-white rounded-full border-2 border-white text-white p-3 bg-opacity-5 hover:border-green-400">
                             <span class="hover:opacity-50 hover:text-green-400">About</span>
