2576103e550c9a0424d50a6d2e8cac8be9fa080c
Loutcho_Q <63863060+loutchoo@users.noreply.github.com>
2022-11-21 13:28:44 +0100

    home arrow reponsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -98,14 +98,14 @@
       </div>
       </div>
     </div>
-    <div class="flex justify-center mt-20">
+    <div class="flex justify-center mt-6 sm:mt-20">
       <a href="/projects">
       <div class="border-2 border-white rounded-full p-2 hover:border-green-300">
         <svg xmlns="http://www.w3.org/2000/svg" class="w-10" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
         </svg></a>
-        
       </div>
     </div>
+    <div class="pb-10"></div>
 </body>
 </html>
